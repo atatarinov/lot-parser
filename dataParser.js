22649e6d59db969ba19d796069c99fad99825e16
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-01-04 21:14:22 -0500

    finished the work

--- dataParser.js
+++ dataParser.js
@@ -6,7 +6,6 @@ const cheerio = require('cheerio');
 
 let artistsNamesArray = [];
 
-
 dir.readFiles(__dirname + '/data/2015-03-18',
   function (err, content, next) {
     if (err) throw err;
@@ -24,11 +23,9 @@ dir.readFiles(__dirname + '/data/2015-03-18',
       let works = this.children[0].data;
       artistObj.works = works;
     });
+
     artistsNamesArray.push(artistObj);
 
-    // console.log('content:', content);
-    // console.log(typeof content);
-    // console.log('_________________');
     next();
   },
   function (err, files) {
@@ -37,62 +34,3 @@ dir.readFiles(__dirname + '/data/2015-03-18',
 
     console.log(artistsNamesArray);
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const request = require('request');
-// const cheerio = require('cheerio');
-
-// const fs = require('fs');
-// const path = require('path');
-
-// function filewalker(dir, done) {
-//   let results = [];
-
-//   fs.readdir(dir, function(err, list) {
-//       if (err) return done(err);
-
-//       var pending = list.length;
-
-//       if (!pending) return done(null, results);
-
-//       list.forEach(function(file){
-//           file = path.resolve(dir, file);
-
-//           fs.stat(file, function(err, stat){
-//             if (err) console.log(err);
-//               // If directory, execute a recursive call
-//               if (stat && stat.isDirectory()) {
-//                   // Add directory to array [comment if you need to remove the directories from the array]
-//                   results.push(file);
-
-//                   filewalker(file, function(err, res){
-//                     if (err) console.log(err);
-//                       results = results.concat(res);
-//                       if (!--pending) done(null, results);
-//                   });
-//               } else {
-//                   results.push(file);
-
-//                   if (!--pending) done(null, results);
-//               }
-//           });
-//       });
-//   });
-// }
-
-// filewalker('./2015-03-18');
